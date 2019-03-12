94da1f3c4f51013a5de1c6133d6dc303f20ffdf9
Alec <alec.jordan.cs@gmail.com>
2019-03-12 12:12:49 -0400

    updates premade db querying

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-let bed = require('./backend_data.json')
+const express = require('express')
+const server = express()
+const db = require('./db')
 
-console.log(bed)
+server.use(express.json())
+
+
+server.get('/', function(req, res) {
+    res.send('server is working')
+})
+
+server.get('/dc', function(req, res) {
+    db('memberz').then(next => res.json(next)).catch(err => console.log(err))
+})
+
+
+server.listen(3000, function() {console.log(`Server listening on port 3000`)})
+
+// let bed = require('./backend_data.json')
+
+// console.log(bed)
